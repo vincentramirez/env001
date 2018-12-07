320d5198d9bd76ed0a56bf76ca34ed2bc4f0962b
Vinnie Ramirez <vinnie.ramirez@gmail.com>
2018-12-07 15:30:14 -0600

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,6 @@
+variable infoblox_username {}
+variable infoblox_password {}
+variable infoblox_host {}
+variable infoblox_sslverify {}
+variable infoblox_usecookies {}
+variable ad_server_domain {}
