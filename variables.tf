f5c0487d805d35a45309dc77cf7ae885f061bc38
Vinnie Ramirez <vinnie.ramirez@gmail.com>
2018-12-07 15:34:38 -0600

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,6 +1,8 @@
+/*
 variable infoblox_username {}
 variable infoblox_password {}
 variable infoblox_host {}
 variable infoblox_sslverify {}
 variable infoblox_usecookies {}
 variable ad_server_domain {}
+*/
